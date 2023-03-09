aac23326704e376a65664e9a17b2563b6e17687c
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-03-09 23:06:37 +0700

    yeyy

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 3a8d677 (todo ver 3)
+>>>>>>> 67625e1 (todo ver 3)
 //====Welcome=====
 let name = document.getElementById("name");
 
@@ -33,3 +40,65 @@ function remove(el) {
 function done(el) {
   el.classList.toggle("done");
 }
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+//introduction
+function nameUser() {}
+
+const ul = document.getElementById("ul"); //kenapa harus const
+const input = document.getElementById("input"); //kenapa harus const
+let todo = []; //kenapa harus buat array
+
+//function untuk membuat list item
+function createTodo() {
+  ul.innerHTML = ""; //untuk merubah input menjadi string
+  todo.forEach((value, index) => {
+    let li = "<li>" + "<span onclick='lineThrought(this)'>" + value + "</span>" + "\n" + "<button onclick='deleteTodo()'>" + "Delete" + "</button>" + "</li>";
+    ul.insertAdjacentHTML("afterbegin", li);
+  });
+}
+
+//function untuk menampilkan hasil membuat list ke browser
+function addTodo() {
+  let todos = input.value;
+  if (todos === "") return;
+  todo.push(todos);
+  input.value = "";
+  createTodo();
+  saveTodo();
+}
+
+//function untuk menghapus item yang sudah ditambahkan
+function deleteTodo(index) {
+  todo.splice(index, 1);
+  createTodo();
+  saveTodo();
+}
+
+//function untuk menggaris bawah
+function lineThrought(el) {
+  el.classList.toggle("done");
+  saveTodo();
+}
+
+//menyimpan list ke local storage
+function saveTodo() {
+  localStorage.setItem("todo", JSON.stringify(todo));
+}
+
+//function untuk memunculkan item saat baru diakses
+function loadTodos() {
+  const savedTodos = localStorage.getItem("todo");
+  if (savedTodos !== null) {
+    todo = JSON.parse(savedTodos);
+    createTodo();
+  }
+}
+
+loadTodos();
+>>>>>>> 517a2d5 (todolist-ver3)
+>>>>>>> 3a8d677 (todo ver 3)
+>>>>>>> 67625e1 (todo ver 3)
